86579c4310da04c5ad59f42c9ac1e0b0e7abd4c9
Ryan <4croor11@solent.ac.uk>
2021-01-15 11:36:12 +0000

    added seeder for deployment

---
+++ seeder.js
@@ -0,0 +1,37 @@
+
+const { MongoClient } = require("mongodb");
+const fs = require("fs").promises;
+const path = require("path");
+const loading = require("loading-cli");
+
+
+/**
+ * constants
+ */
+// const uri = "mongodb://localhost:27017/focus";
+const uri = "mongodb+srv://admin:hkHg9xpbAg2cNY4@cluster0.kh98s.mongodb.net/focus?retryWrites=true&w=majority"
+const client = new MongoClient(uri);
+
+async function main() {
+  try {
+    await client.connect();
+    const db = client.db();
+
+    /**
+     * If existing records then delete the current collections
+     */
+    if (results) {
+      db.dropDatabase();
+    }
+
+    load.stop();
+
+
+    process.exit();
+  } catch (error) {
+    console.error("error:", error);
+    process.exit();
+  }
+}
+
+main();
